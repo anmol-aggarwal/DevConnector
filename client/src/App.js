0edf6c2fe0bca424b83234cff0d6a9c40b754c23
Anmol_Aggarwal <aggarwalanmol2507@gmail.com>
2022-03-29 13:17:48 +0530

    Dashboard completed

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import { loadUser } from './actions/auth';
 //Redux
 import { Provider } from 'react-redux';
@@ -44,6 +47,7 @@ const App = () => {
         <Navbar />
         <Alert />
         <Routes>
+          <Route path="/" element={<Landing />} />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
           <Route
@@ -53,6 +57,18 @@ const App = () => {
           <Route
             path="create-profile"
             element={<PrivateRoute component={CreateProfile} />}
+          />
+          <Route
+            path="edit-profile"
+            element={<PrivateRoute component={EditProfile} />}
+          />
+          <Route
+            path="add-experience"
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path="add-education"
+            element={<PrivateRoute component={AddEducation} />}
           />
         </Routes>
       </Router>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 import {
+  ACCOUNT_DELETED,
+  CLEAR_PROFILE,
   GET_PROFILE,
   PROFILE_ERROR,
+  UPDATE_PROFILE
 } from './types';
 
 //get current user's profile
@@ -56,3 +59,135 @@ export const createProfile =
       });
     }
   };
+
+//Add experience
+export const addExperience = (formData, navigate) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put(
+      'http://localhost:5005/api/profile/experience',
+      formData,
+      config
+    );
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Added', 'success'));
+
+    navigate('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Add education
+export const addEducation = (formData, navigate) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put(
+      'http://localhost:5005/api/profile/education',
+      formData,
+      config
+    );
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Added', 'success'));
+
+    navigate('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete Experience
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(
+      `http://localhost:5005/api/profile/experience/${id}`
+    );
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete Education
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(
+      `http://localhost:5005/api/profile/education/${id}`
+    );
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert('Education Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete account and profile
+export const deleteAccount = () => async (dispatch) => {
+  try {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+      await axios.delete(`http://localhost:5005/api/profile`);
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+      dispatch(setAlert('Your account has been permanently deleted'));
+    }
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
